ef45524ad0629ea29a7ae48190f205f86474ae4f
@Abhisec007 <64474185+Abhisec007@users.noreply.github.com>
2026-09-09 22:00:02 +0530

    Initialize reading_input.java with Scanner setup
    Added a basic structure for reading input using Scanner.

---
+++ reading_input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // We can now use the scanner tool.
+        // This tool is used to read input.
+    }
+}
