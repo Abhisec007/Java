ab1eb23ca04bd30bd7dac8ca8e04ffde83ed46a3
@Abhisec007 <64474185+Abhisec007@users.noreply.github.com>
2026-09-16 18:56:55 +0530

    Add user input functionality to Program class

--- Variables.java
+++ Variables.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
 
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write text and press enter ");
+        String text = scanner.nextLine();
+        System.out.println("You wrote " + text);
+    }
+}
