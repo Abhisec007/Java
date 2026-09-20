a33e2905b4edf48896098efdbb45dba98c3004b0
@Abhisec007 <64474185+Abhisec007@users.noreply.github.com>
2026-09-20 19:16:27 +0530

    Rename class and update print statement
    Correct class name and fix print statement casing.

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,6 @@
-public class Sandbox {
+public class helloworld {
     public static void main(String[] args) {
         // Write your program here
-
+        system.out.println("Hello World!")
     }
 }
