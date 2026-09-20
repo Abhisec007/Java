c456a66b36130451466bd5d70af7e0a225e41785
@Abhisec007 <64474185+Abhisec007@users.noreply.github.com>
2026-09-20 19:35:09 +0530

    Correct datatypes class definition and syntax
    Fix syntax errors in the datatypes class.

---
+++ datatypes.java
@@ -0,0 +1,11 @@
+public main datatypes{
+  public static void main(Strings[] args){
+    int a=10;
+    String name="Arun";
+    float f= 20.4f;
+    double d= 23.44;
+    char e="xyz"
+    boolean b=true;
+    
+  }
+}
