48c1fe47d6727e2e5fcfebee3d801808c62a44a0
@Abhisec007 <64474185+Abhisec007@users.noreply.github.com>
2026-09-20 19:25:29 +0530

    Modify main method and add variable declarations
    Updated main method signature and added variable declarations.

--- Variables.java
+++ Variables.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
+        int a=10;
+        String text = "contains text";
+        int wholeNumber = 123;
+        double floatingPoint = 3.141592653;
+        boolean trueOrFalse = true;
+        float a=10.4f;
+        
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Write text and press enter ");
